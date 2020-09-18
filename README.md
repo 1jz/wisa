eaeae3fb1f979c20c0dec558dcff3fb61e5053c9
1jz <philipgolovin@gmail.com>
2020-09-18 03:10:08 -0400

    added flags, updated comments and added more instructions

--- README.md
+++ README.md
@@ -5,15 +5,22 @@ Who Is Still Alive: a broken link checker
 
 ```go build wisa.go```
 
-# Usage
-type `wisa` for usage or: 
+Add wisa to your PATH or use it directly.
 
-```wisa [file]```
+# Usage
+type `wisa --help` for usage or: 
+
+```wisa -f [file]```
+
+for verbose output (error logs)
+
+```wisa -f [file] -v```
 
 # Features
 
 - Uses go routines to check for dead/broken links
 - Uses `HEAD` requests for optimization
-- 200-299 status codes are considered a PASS
-- 403 emits a WARN
-- All other codes are considered DEAD
+- `200-299` status codes are considered a `PASS`
+- `403` emits a `WARN`
+- All other codes are considered `DEAD`
+- Uses flags for passing filename and for verbose output
--- wisa.go
+++ wisa.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -9,15 +10,24 @@ import (
 	"sync"
 )
 
+// flag globals
+var (
+	filenamePtr *string
+	verbosePtr  *bool
+)
+
 func checkLink(wg *sync.WaitGroup, url string) {
 
+	// defered function is run when surrounding functions are completed
 	defer wg.Done()
 
 	// use HEAD request https://golang.org/pkg/net/http/
 	resp, err := http.Head(url)
 
 	if err != nil {
-		fmt.Println(err)
+		if *verbosePtr {
+			fmt.Println(err)
+		}
 	} else {
 		// Status codes https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 		if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
@@ -31,37 +41,48 @@ func checkLink(wg *sync.WaitGroup, url string) {
 }
 
 func main() {
-	// regex for finding urls
-	// modification of this ECMAscript regex ported to work with go regexp
+
+	// https://golang.org/pkg/flag/
+	filenamePtr = flag.String("f", "", "filename input (required)") // filename input
+	verbosePtr = flag.Bool("v", false, "verbose output")            // (error logs)
+
+	flag.Parse()
+
+	// regex for finding urls https://golang.org/pkg/regexp/#Regexp
+	// thank you https://gist.github.com/dperini/729294 for the regex.
+	// modified from ECMAscript regex, ported to work with Go regexp
 	r := regexp.MustCompile(`(?:(?:(?:https?|ftp):)\/\/)(?:\S+(?::\S*)?@)?(?:(x??!(?:10|127)(?:\.\d{1,3}){3})(x??!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(x??!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\x{00a1}-\x{ffff}][a-z0-9\x{00a1}-\x{ffff}_-]{0,62})?[a-z0-9\x{00a1}-\x{ffff}]\.)+(?:[a-z\x{00a1}-\x{ffff}]{2,}\.?))(?::\d{2,5})?([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?`)
 	// seperate regex for IP urls since the above did not work
-	r_ip := regexp.MustCompile(`(?:(?:(?:https?|ftp):)\/\/)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d{2,5})?`)
+	rIP := regexp.MustCompile(`(?:(?:(?:https?|ftp):)\/\/)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d{2,5})?`)
 
 	// check if arg is passed
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: wisa [file]")
+	if len(*filenamePtr) <= 0 {
+		fmt.Println("Usage: wisa -f [file]")
 		os.Exit(1)
-	} else if len(os.Args) > 2 {
-		fmt.Println("Please enter a single filename.")
-		os.Exit(2)
+	}
+
+	// notift if -v flag is passed
+	if *verbosePtr {
+		fmt.Println("verbose output enabled...")
 	}
 
 	// Open a file (read-only) https://golang.org/pkg/os/#Open
-	file, err := os.Open(os.Args[1])
+	fmt.Printf("Reading %s...\n", *filenamePtr)
+	file, err := os.Open(*filenamePtr)
 
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(3)
+		os.Exit(2)
 	}
 
 	// read all data in to byte slice
 	text, err := ioutil.ReadAll(file)
 
 	// find all regex matches and in converted byte data and concat both string slices into single slice
-	text_urls := r.FindAllString(string(text), -1)
-	ip_urls := r_ip.FindAllString(string(text), -1)
+	textUrls := r.FindAllString(string(text), -1)
+	ipUrls := rIP.FindAllString(string(text), -1)
 
-	urls := append(text_urls, ip_urls...)
+	urls := append(textUrls, ipUrls...)
 
 	// stop reading file
 	file.Close()
@@ -75,5 +96,6 @@ func main() {
 		go checkLink(&wg, url)
 	}
 
+	// wait for go routines to finish
 	wg.Wait()
 }
