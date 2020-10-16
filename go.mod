a02c9e58ef6fe17cbfcfdab44ede9b5342ccf2d2
matthew stewardson <mstewardson@myseneca.ca>
2020-10-16 00:36:27 -0400

    Added the ignore url feature

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/gookit/color v1.3.1
 	github.com/spf13/pflag v1.0.5
+	mvdan.cc/xurls v1.1.0
+	mvdan.cc/xurls/v2 v2.2.0
 )
--- wisa.go
+++ wisa.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 	"time"
 
@@ -17,9 +20,10 @@ import (
 
 // flag globals
 var (
-	filenamePtr *string
-	verbosePtr  *bool
-	jsonPtr     *bool
+	filenamePtr   *string
+	verbosePtr    *bool
+	jsonPtr       *bool
+	ignoreFilePtr *bool
 )
 
 // RequestResult is a struct for storing urls and status codes
@@ -79,12 +83,28 @@ func checkLink(wg *sync.WaitGroup, url string) (RequestResult, int, error) {
 	return r, status, reqErr
 }
 
+//Goes through the url list and removes any url that matches with the ignore url list
+func ignoreURL(url []string, ignoreList []string) []string {
+	//Loop through the url list
+	for i := 0; i < len(url); i++ {
+		//Loop through the ignore url list
+		for k := 0; k < len(ignoreList); k++ {
+			//If the beginning of the url matches the ignoreList value, set the url value to ""
+			if strings.HasPrefix(url[i], ignoreList[k]) == true {
+				url[i] = ""
+			}
+		}
+	}
+	return url
+}
+
 func main() {
 
 	// https://github.com/spf13/pflag
 	filenamePtr = flag.StringP("file", "f", "", "filename input (required)") // filename input
 	verbosePtr = flag.BoolP("version", "v", false, "verbose output")         // (error logs)
 	jsonPtr = flag.BoolP("json", "j", false, "json output")                  // turns off verbose output
+	ignoreFilePtr = flag.BoolP("ignore", "i", false, "ignores certain urls based off a text file")
 
 	flag.Parse()
 
@@ -95,83 +115,127 @@ func main() {
 	// seperate regex for IP urls since the above did not work
 	rIP := regexp.MustCompile(`(?:(?:(?:https?|ftp):)\/\/)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:\d{2,5})?`)
 
+	//String array holding the list of urls to ignore
+	ignoreURLs := []string{}
+
 	// check if arg is passed
-	if len(*filenamePtr) == 0 {
+	if len(os.Args) == 1 {
 		fmt.Println("Usage: wisa -f [file]")
 		os.Exit(1)
-	}
-
-	// notify if -v flag is passed
-	if *verbosePtr && !*jsonPtr {
-		fmt.Println("verbose output enabled...")
-	}
-
-	// Open a file (read-only) https://golang.org/pkg/os/#Open
-	if !*jsonPtr {
-		fmt.Printf("Reading %s...\n", *filenamePtr)
-	}
-
-	file, err := os.Open(*filenamePtr)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
-
-	// read all data in to byte slice
-	text, err := ioutil.ReadAll(file)
-
-	// find all regex matches and in converted byte data and concat both string slices into single slice
-	textUrls := r.FindAllString(string(text), -1)
-	ipUrls := rIP.FindAllString(string(text), -1)
-
-	urls := append(textUrls, ipUrls...)
-
-	// stop reading file
-	file.Close()
-
-	urls = removeDuplicate(urls)
-
-	// create workgroup to ensure all routines finish https://golang.org/pkg/sync/#WaitGroup
-	var wg sync.WaitGroup
-
-	// json output stuff
-	var jsonSlice []RequestResult
-	var mut sync.Mutex
-	finalExit := 0
-
-	// check if urls found are alive
-	for _, url := range urls {
-		wg.Add(1)
-		go func(url string) {
-			res, status, err := checkLink(&wg, url)
-
-			if err == nil {
-				mut.Lock()
-				jsonSlice = append(jsonSlice, res)
-				mut.Unlock()
+	} else {
+		//Ignore
+		if *ignoreFilePtr {
+			//Get the file path
+			ignoreFilePath := flag.Args()[0]
+			fmt.Printf("Reading %s...\n", ignoreFilePath)
+			//Open the file
+			fileIgn, err := os.Open(ignoreFilePath)
+			//In case of panic
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
 			}
-
-			if status == 3 {
-				mut.Lock()
-				finalExit = status
-				mut.Unlock()
+			//Close the file
+			defer fileIgn.Close()
+			//Create new scanner
+			scanner := bufio.NewScanner(fileIgn)
+			//Loop through each line of the file
+			for scanner.Scan() {
+				line := scanner.Text()
+				//If the starting character is not "#"
+				startChar := string(line[0])
+				if startChar != "#" {
+					//Match agansit regrex patterns
+					UrlsFromLine := r.FindAllString(string(line), -1)
+					ipUrlsFromLine := rIP.FindAllString(string(line), -1)
+					UrlsFromLine = append(UrlsFromLine, ipUrlsFromLine...)
+					ignoreURLs = append(ignoreURLs, UrlsFromLine...)
+				}
 			}
-		}(url)
-	}
+			//Remove any dupes
+			removeDuplicate(ignoreURLs)
+			if err := scanner.Err(); err != nil {
+				log.Fatal(err)
+			}
+		}
 
-	// wait for go routines to finish
-	wg.Wait()
+		// notify if -v flag is passed
+		if *verbosePtr && !*jsonPtr {
+			fmt.Println("verbose output enabled...")
+		}
 
-	if *jsonPtr {
-		urlsJ, err := json.Marshal(jsonSlice)
+		// Open a file (read-only) https://golang.org/pkg/os/#Open
+		if !*jsonPtr {
+			fmt.Printf("Reading %s...\n", *filenamePtr)
+		}
+
+		file, err := os.Open(*filenamePtr)
 
 		if err != nil {
 			fmt.Println(err)
-		} else {
-			fmt.Println(string(urlsJ))
+			os.Exit(2)
 		}
 
+		// read all data in to byte slice
+		text, err := ioutil.ReadAll(file)
+
+		// find all regex matches and in converted byte data and concat both string slices into single slice
+		textUrls := r.FindAllString(string(text), -1)
+		ipUrls := rIP.FindAllString(string(text), -1)
+
+		urls := append(textUrls, ipUrls...)
+
+		// stop reading file
+		file.Close()
+
+		urls = removeDuplicate(urls)
+
+		//Call the ignoreURLs func to remove any urls that match the list
+		if *ignoreFilePtr == true {
+			urls = ignoreURL(urls, ignoreURLs)
+		}
+
+		// create workgroup to ensure all routines finish https://golang.org/pkg/sync/#WaitGroup
+		var wg sync.WaitGroup
+
+		// json output stuff
+		var jsonSlice []RequestResult
+		var mut sync.Mutex
+		finalExit := 0
+
+		// check if urls found are alive
+		for _, url := range urls {
+			wg.Add(1)
+			go func(url string) {
+				res, status, err := checkLink(&wg, url)
+
+				if err == nil {
+					mut.Lock()
+					jsonSlice = append(jsonSlice, res)
+					mut.Unlock()
+				}
+
+				if status == 3 {
+					mut.Lock()
+					finalExit = status
+					mut.Unlock()
+				}
+			}(url)
+		}
+
+		// wait for go routines to finish
+		wg.Wait()
+
+		if *jsonPtr {
+			urlsJ, err := json.Marshal(jsonSlice)
+
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(string(urlsJ))
+			}
+
+		}
+		os.Exit(finalExit)
 	}
-	os.Exit(finalExit)
 }
